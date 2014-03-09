e001f9288b7735c6e8af30219ac61dedbe67995d
Kemal Eren <kemal@kemaleren.com>
2014-03-09 12:28:07 -0700

    saving recent changes

--- sysgenb.py
+++ sysgenb.py
@@ -1,10 +1,12 @@
+from __future__ import division
+
 import os
 
 import numpy as np
 from scipy.stats import spearmanr
 import pandas as pd
 
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNetCV, LinearRegression
 from sklearn.cross_validation import KFold
 
 DATA_DIR = "../data"
@@ -42,7 +44,19 @@ def read_gold_standard_data():
     return genotype, expression, percent_present, scale_factor
 
 
-def make_features(genotype, expression):
+def read_name_file(filename):
+    with file(filename) as f:
+        return f.read().split('\n')
+
+
+def get_names():
+    gene_file = os.path.join(DATA_DIR, 'DREAM5_SysGenB_GenotypeMarkerIDs.csv')
+    expression_file = os.path.join(DATA_DIR,
+                                   'DREAM5_SysGenB_ExpressionProbeIDs.csv')
+    return read_name_file(gene_file), read_name_file(expression_file)
+
+
+def make_data(genotype, expression):
     """concatenate genotype and expression data
 
     Returns the feature array and a list of feature names.
@@ -53,51 +67,95 @@ def make_features(genotype, expression):
     s = expression.std(axis=0, ddof=1)
     expression = (expression - m) / s
 
-    genotype_names = list("genotype_{}".format(i)
-                          for i in range(genotype.shape[1]))
-    expression_names = list("expression_{}".format(i)
-                            for i in range(expression.shape[1]))
+    genotype_names, expression_names = get_names()
     feats = np.hstack((genotype, expression))
     names = genotype_names + expression_names
 
+    return genotype, genotype_names
     return feats, names
+
+
+def make_rates(coefs):
+    return (coefs != 0).sum(axis=0) / coefs.shape[0]
+
+
+def train_model(data, target, n_iter=3, rate=0.5):
+    """Bootstraps, trains ElasticNetCV model, selects features, and
+    trains final linear regression model.
+
+    Returns model and selected features.
+
+    """
+    coefs = []
+    for i in range(n_iter):
+        print "bootstrap iter {}".format(i)
+        indices = np.random.choice(len(data), size=len(data), replace=True)
+        sample_data = data[indices]
+        sample_target = target[indices]
+        model = ElasticNetCV(l1_ratio=[.1, .5, .7, .9, .95, .99, 1],
+                             max_iter=10000)
+        model.fit(sample_data, sample_target)
+        coefs.append(model.coef_)
+    coefs = np.vstack(coefs)
+    rate_selected = make_rates(coefs)
+    selected = np.nonzero(rate_selected >= rate)[0]
+    model = LinearRegression()
+    model.fit(data[:, selected], target)
+    return model, selected, coefs
+
+
+def do_fold(name, data, target, data_test, target_test):
+    model, selected, coefs = train_model(data, target)
+    preds = model.predict(data_test[:, selected])
+    n_feats = len(selected)
+    r, p = spearmanr(target_test, preds)
+    print "{} n_features : {}, r: {}, p: {}".format(
+        name, n_feats, r, p)
+    return coefs, p
 
 
 if __name__ == "__main__":
     genotype, expression, percent_present, scale_factor = \
         read_training_data()
-    features, feature_names = make_features(genotype, expression)
+    data, feature_names = make_data(genotype, expression)
 
-    model_present = Lasso(alpha=0.1)
-    model_scale = Lasso(alpha=0.1)
-
-    kf = KFold(len(features))
+    kf = KFold(len(data), n_folds=2, random_state=0)
 
     k = 0
+
+    all_coefs_percent = []
+    all_coefs_scale = []
     for train_index, test_index in kf:
         print "fold: {}".format(k)
         k += 1
-        train_data = features[train_index]
-        train_present = percent_present[train_index]
-        train_scale = scale_factor[train_index]
+        data_train = data[train_index]
+        percent_present_train = percent_present[train_index]
+        scale_factor_train = scale_factor[train_index]
 
-        test_data = features[test_index]
-        test_present = percent_present[test_index]
-        test_scale = scale_factor[test_index]
+        data_test = data[test_index]
+        percent_present_test = percent_present[test_index]
+        scale_factor_test = scale_factor[test_index]
 
-        model_present.fit(train_data, train_present)
-        preds_present = model_present.predict(test_data)
-        present_n_feats = np.count_nonzero(model_present.coef_)
-        present_r, present_p = spearmanr(test_present, preds_present)
-        print "percent_present n_features : {}, r: {}, p: {}".format(
-            present_n_feats, present_r, present_p)
+        coefs_percent, p_percent = do_fold(
+            'percent present', data_train, percent_present_train, data_test,
+            percent_present_test)
+        all_coefs_percent.append(coefs_percent)
 
-        model_scale.fit(train_data, train_scale)
-        preds_scale = model_scale.predict(test_data)
-        scale_n_feats = np.count_nonzero(model_scale.coef_)
-        scale_r, scale_p = spearmanr(test_scale, preds_scale)
-        print "scale_factor n_features : {}, r: {}, p: {}".format(
-            scale_n_feats, scale_r, scale_p)
+        coefs_scale, p_scale = do_fold(
+            'scale_factor', data_train, scale_factor_train, data_test,
+            scale_factor_test)
+        all_coefs_scale.append(coefs_scale)
 
-        score = -np.log(present_p * scale_p)
+        score = -np.log(p_percent * p_scale)
         print "overall score: {}".format(score)
+
+    rates_percent = make_rates(np.vstack(all_coefs_percent))
+    rates_scale = make_rates(np.vstack(all_coefs_scale))
+
+    # save features and their selection rate
+    df = pd.DataFrame(zip(feature_names, rates_percent, rates_scale),
+                      columns=('feature', 'rate_percent_present',
+                               'rate_scale'))
+    df.to_csv('feature_selection_rates.csv')
+
+    # make biplots
